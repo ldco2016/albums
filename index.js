ed915ddcc1393ce8a5864ffe92d7e7b52feb5671
danale <danale@danales-MacBook-Pro.local>
2018-02-26 00:00:06 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Import a library to help create a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+import Header from './src/components/header';
+
+// Create a component
+const App = () => (
+  <Header />
+);
+
+// Render it to a device
+AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+// Import a library to help create a component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// Make a component
+const Header = () => {
+  const { textStyle } = styles;
+
+  return <Text style={textStyle}>Albums!</Text>;
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  }
+};
+
+// Make the component available to other parts of the app
+export default Header;
