d993968a7c7cc6d3ef0cde256046a724c3cd13d9
danale <danale@danales-MacBook-Pro.local>
2018-02-28 09:58:24 -0500

    Implemented rendering a list of components

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,27 +1,45 @@
+// Import a library to help create a component
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import axios from 'axios';
 
+// ES6 Class Component
 class AlbumList extends Component {
   // initializing component level state
   // to get access to data and rerender
+  // this.state.albums equal to empty array
   state = { albums: [] };
 
+  // lifecycle method to
+  // fetch data as soon as app boots up
   componentWillMount() {
     axios
       .get('https://rallycoding.herokuapp.com/api/music_albums')
+      // called once http request is complete with response and data
+      // inside object in chrome console dictated use of response.data
+      // passing albums object to setState because updating state
+      // this.state.albums no longer equal to empty array
+      // setState need not be defined, comes with Component class
       .then(response => this.setState({ albums: response.data }));
   }
 
+  renderAlbums() {
+    return this.state.albums.map(album => <Text>{album.title}</Text>);
+  }
+
   render() {
+    // result of adding state code
     console.log(this.state);
 
     return (
       <View>
-        <Text>Album List!!</Text>
+        <Text>
+          {this.renderAlbums()}
+        </Text>
       </View>
     );
   }
 }
 
+// Make the component available to other parts of the app
 export default AlbumList;
